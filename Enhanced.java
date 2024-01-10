b550519bfb8391575e4ccf5f43c68ac697c6656d
ZachMartin24 <117095429+ZachMartin24@users.noreply.github.com>
2024-01-10 11:13:55 -0500

    Add files via upload

---
+++ Enhanced.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) throws Exception {
+
+        double[] numbers = { 2.2, 1.0, 3.7, 4.1, 88.0 };
+
+        for (int i = 0; i < numbers.length; i++) {
+            double element = numbers[i];
+            System.out.println(element);
+        }
+    }
+}
